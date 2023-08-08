5d2b4d9851b9534a1b38c48ae32d511d41f40e13
muyouming <muyouming@gmail.com>
2023-08-08 17:21:01 +0900

    Update get_client_id.py

--- actions/get_client_id.py
+++ actions/get_client_id.py
@@ -4,5 +4,5 @@ from lib import action
 class KeycloakGetClientAction(action.KeycloakBaseAction):
     def run(self, client_name):
 
-        client_id = self.keycloak_admin.get_client_id(client_name=client_name)
+        client_id = self.keycloak_admin.get_client_id(client_name)
         return client_id
